53fa10edcf7880a057aa1a34215e1f9857c27334
Rod Fetalvero <botsot.felix@gmail.com>
2025-04-20 09:14:54 +0000

    update solution

--- index.js
+++ index.js
@@ -24,6 +24,18 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', (req, res) => {
+  const ipaddress = req.ip;
+  const language = req.header(`accept-language`);
+  const software = req.header(`user-agent`);
+
+  res.json({
+    ipaddress: ipaddress,
+    language: language,
+    software: software,
+  });
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
